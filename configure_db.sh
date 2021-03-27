70e356dc4c90244b7e87e4c8429ebb5e04aa0f06
mekhatib <melkhatib@ziggo.nl>
2021-03-27 21:44:52 +0100

    Update configure_db.sh

--- configure_db.sh
+++ configure_db.sh
@@ -3,6 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y postgresql-client
 
-wget -O database.sql https://github.com/nicholasjackson/gopher_search/releases/download/v0.1/database.sql
+wget -O database.sql https://github.com/mekhatib/DBtest/blob/main/database.sql
 
 PGPASSWORD=$3 psql "sslmode=disable host=$1 port=5432 dbname=gopher_search_production" --username=$2 < ./database.sql
